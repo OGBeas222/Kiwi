9807455e096d640f30754ed44f621a23d2bd383e
OGBeas222 <zholudev111@gmail.com>
2022-06-29 09:38:39 +0300

    changes in config

---
+++ app/blocks/develop/header/deps.js
@@ -0,0 +1,15 @@
+//
+// 'use strict'
+//
+// module.exports = {
+//
+// 	nodes: [],
+//
+// 	modules: [
+// 		{
+// 			from:'app/blocks/develop/header/',
+// 			import: '_global.scss'
+// 		}
+// 	],
+//
+// }
--- app/config.js
+++ app/config.js
@@ -1,4 +1,3 @@
-
 'use strict'
 
 
@@ -13,20 +12,37 @@ module.exports = {
 	use: {
 		templates: '.html',
 		scripts: '.js',
-		styles: '.css',
+		styles: '.scss',
 	},
 
 	build: {
-		imagemin: [ 'png', 'jpg' ],
+		imagemin: [ 'png', 'jpg', 'svg', 'gif' ],
 		sourcemaps: [ 'js', 'css' ],
 		autoprefixer: [ 'last 3 versions', 'ie 10', 'ie 11' ],
+		mainBundle: 'app', // название основного бандла проекта
+		mainLevel: 'develop',
+		globalStyles:
+			[
+			'app/blocks/develop/app/style/_vars.scss',
+			'app/blocks/develop/app/style/_reset.scss',
+			'app/blocks/develop/app/style/_global.scss',
+			'app/blocks/develop/app/style/_fonts.scss'
+			],
+		BEML: true,
+		pugMap: false,
+		addVersions: true,
+		HTMLRoot: './',
 	},
 
 	autoCreate: {
 		onlyOnWatch: true,
-		files: [ '.css' ],
-		levels: [ 'develop' ],
-		ignoreNodes: [ 'symbol', /_no_js/i ],
+		// folders: [ 'img', 'assets' ],
+		// files: [ '.scss', '.js' ],
+		// levels: [ 'common' ],
+		ignoreStyle: [ /[a-z\d](_|--)[a-z\d]/i ], // игнорируем модификаторы при создании стилей
+		ignoreScript: [ /[a-z\d](_|--)[a-z\d]/i  ], // игнорируем модификаторы при создании скриптов
+		ignoreTemplate: ['develop'], // по аналогии можно указать и для шаблонов
+		ignoreNodes: [ /__[\w]/i ],
 	},
 
 	dist: {
@@ -51,8 +67,52 @@ module.exports = {
 	},
 
 	HTMLBeautify: {
-		preserve_newlines: false,
+		indent_size: 4, // размер отступа
+		indent_char: ' ', // знак отступа
+		indent_with_tabs: true, // отступ табами
+		indent_inner_html: true, // нужны ли отступы внутри тега html
+		end_with_newline: false, // нужна ли пустая строка в конце файла
+		extra_liners: [], // список тегов с пустой строкой перед собой
+		preserve_newlines: true, // можно ли использовать пустые строки
+		max_preserve_newlines: 2, // максимальное кол-во пустых строк подряд
+		inline: [], // список строчных тегов (по умолчанию по стандарту w3c)
+		unformatted: [], // список тегов, которые не нужно форматировать (по умолчанию inline)
+		content_unformatted: [ 'pre', 'textarea' ], // список тегов, у которых не нужно форматировать содержимое
 	},
 
+	fastMake: {
+		b: ['.js','.scss','.html','img']
+	},
+
+	optimization: {
+
+		jpg: {
+			progressive: true,
+			arithmetic: false,
+		},
+
+		png: {
+			optimizationLevel: 5, // можно указать 0-7
+			bitDepthReduction: true,
+			colorTypeReduction: true,
+			paletteReduction: true,
+		},
+
+		gif: {
+			optimizationLevel: 1, // можно указать 1-3
+			interlaced: true
+		},
+
+		// Для svg нужно указать массив c настройками!
+		svg: [
+			{ cleanupIDs: false },
+			{ removeViewBox: false },
+			{ mergePaths: false },
+		],
+
+		// Тут можно указать названия (без расширения), которые не нужно оптимизировать
+		ignore: []
+
+	},
 }
 
